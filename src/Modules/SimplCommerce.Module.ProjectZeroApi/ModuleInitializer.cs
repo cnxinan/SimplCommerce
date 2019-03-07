e9cf92fae1628a8058531d3b84d8dd100bd0eacb
Cedric.Chen <cedri.chen@wendywuchina.com>
2019-03-07 17:51:26 +0800

    0000: update doc info.

---
+++ src/Modules/SimplCommerce.Module.ProjectZeroApi/ModuleInitializer.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using SimplCommerce.Infrastructure;
+using SimplCommerce.Module.Core.Events;
+
+namespace SimplCommerce.Module.ProjectZeroApi
+{
+    public class ModuleInitializer : IModuleInitializer
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+        }
+    }
+}
