3482cb01b99c7d5f6651efb3183c4dc37a9d1d8c
Cedric.Chen <cedri.chen@wendywuchina.com>
2019-03-11 16:24:07 +0800

    0000: Add project zero api project.

---
+++ src/Modules/SimplCommerce.Module.ProjectZeroApi/Areas/ProjectZeroApi/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimplCommerce.Module.ProjectZeroApi.Areas.ProjectZeroApi
+{
+    [Area("ProjectZeroApi")]
+    [Route("api/projectzero")]
+    public class TestApiController: Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new { TestResult = "Test successful!" });
+        }
+    }
+}
